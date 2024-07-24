dd214e81a22c6796194aefe65df101be8887cdae
khushichawla <khushichawlaemails@gmail.com>
2024-07-24 18:01:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var typed = new Typed(".text", {
+    strings: ["Fresh Graduate", "Software Engineer", "Frontend Developer", "Backend Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
