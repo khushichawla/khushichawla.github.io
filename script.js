83a05fda17f5ca2bca1b64c161b49fd4942c85e8
khushichawla <khushichawlaemails@gmail.com>
2024-07-24 23:53:37 +0530

    added qualifications section

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ var typed = new Typed(".text", {
     loop: true
 })
 
-const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]')
+// const tabs = document.querySelectorAll('[data-target]'),
+//     tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target)
+// tabs.forEach(tab => {
+//     tab.addEventListener('click', () => {
+//         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('qualification_active')
-        })
-        target.classList.add('qualification_active')
+//         tabContents.forEach(tabContent => {
+//             tabContent.classList.remove('qualification_active')
+//         })
+//         target.classList.add('qualification_active')
 
-        tabs.forEach(tab => {
-            tab.classList.remove('qualification_active')
-        })
-        tab.classList.add('qualification_active')
-    })
-})
+//         tabs.forEach(tab => {
+//             tab.classList.remove('qualification_active')
+//         })
+//         tab.classList.add('qualification_active')
+//     })
+// })
